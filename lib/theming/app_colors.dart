d2c153ea41d176261847b20561d5da1a189fb220
Nermen77 <nermeenzaitoon@gmail.com>
2025-07-21 18:16:21 +0300

    Add required colors and create class for theming.

---
+++ lib/theming/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color primaryLightColor = Color(0xffA98C6F);
+  static const Color whiteColor = Color(0xffffffff);
+  static const Color blackColor = Color(0xff000000);
+  static const Color lightBeigeColor = Color(0xffebe5df);
+  static const Color primaryDarkColor = Color(0xff895b2b);
+  static const Color darkBeigeColor = Color(0xff8e7e6b);
+}
---
+++ lib/theming/my_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:tick_done_app/theming/app_colors.dart';
+
+class MyTheme {
+  static final ThemeData lightTheme = ThemeData(
+    primaryColor: AppColors.primaryLightColor,
+    scaffoldBackgroundColor: AppColors.whiteColor,
+    appBarTheme: AppBarTheme(
+
+    ),
+
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+
+    ),
+
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+
+    ),
+
+    textTheme: TextTheme(
+
+    ),
+
+  );
+}
