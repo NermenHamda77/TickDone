e5cdf7b4ce5982dbe5ef9e65e6b90c9bad614ca4
Nermen77 <nermeenzaitoon@gmail.com>
2025-07-26 17:48:41 +0300

    Integrate firebase with my project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tick_done_app/home/home_screen.dart';
@@ -5,12 +8,21 @@ import 'package:tick_done_app/new_task/add_new_task_screen.dart';
 import 'package:tick_done_app/providers/app_config_provider.dart';
 import 'package:tick_done_app/theming/my_theme.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-void main() {
-  runApp(
-      ChangeNotifierProvider(
-        create: (context) => AppConfigProvider(),
-        child: const MyApp()
-      ));
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid ?
+     await Firebase.initializeApp(
+       options: FirebaseOptions(
+           apiKey: 'AIzaSyAU-X3Q3pAxlabMrum9bp_CVKIXmUsxSmM',
+           appId: 'com.example.tick_done_app',
+           messagingSenderId: '948642296882',
+           projectId: 'tick-done-app'
+       )
+
+     ) : await Firebase.initializeApp();
+  runApp(ChangeNotifierProvider(
+      create: (context) => AppConfigProvider(), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -25,11 +37,9 @@ class MyApp extends StatelessWidget {
       routes: {
         HomeScreen.routeName: (context) => HomeScreen(),
         NewTaskScreen.routeName: (context) => NewTaskScreen(),
-
       },
       themeMode: provider.appMode,
       darkTheme: MyTheme.darkTheme,
-
       initialRoute: HomeScreen.routeName,
       locale: Locale(provider.appLanguage),
       debugShowCheckedModeBanner: false,
